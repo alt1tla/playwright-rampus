059136c0e2937992c84c9e60cbd61e9f7c4b21c9
alt1tla <GerdzhikovaVeronWench@gmail.com>
2026-06-20 21:11:35 +0300

    refactor auth

---
+++ tests/auth/auth.test.js
@@ -0,0 +1,36 @@
+//@ts-check
+import { test, expect } from "@playwright/test";
+
+test.use({ storageState: { cookies: [], origins: [] } });
+test.describe("auth", () => {
+  test("unathorized redirecting & check ui", async ({ page }) => {
+    await page.goto("/");
+    await expect(page).toHaveURL(/auth/);
+    await expect(page.getByRole("link", { name: "Рампус" })).toBeVisible();
+    await expect(page.getByText("Логин или почта")).toBeVisible();
+    await expect(page.getByText("Пароль")).toBeVisible();
+    await expect(page.getByRole("button", { name: "Войти" })).toBeVisible();
+    const registrationLink = page.getByRole("main").getByRole("link");
+    await expect(registrationLink).toBeVisible();
+    await registrationLink.click();
+    await expect(page).toHaveURL(/reg/);
+  });
+
+  test("wrong data filled behavior", async ({ page }) => {
+    await page.goto("/auth");
+    const usernameField = await page.getByRole("textbox", {
+      name: "Логин или почта",
+    });
+    const passwordFiled = await page.getByRole("textbox", { name: "Пароль" });
+    const loginInButton = await page.getByRole("button", { name: "Войти" });
+    const notifyTip = await page.locator("#auth__notify");
+
+    await loginInButton.click();
+    await expect(notifyTip).toHaveText("Все поля должны быть заполнены");
+
+    await usernameField.fill("mayhem");
+    await passwordFiled.fill("abracadabra");
+    await loginInButton.click();
+    await expect(notifyTip).toHaveText("Неверный логин, почта или пароль");
+  });
+});
